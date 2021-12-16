047b6eadd758609820e3440b871cb2a01ff87fc8
rodrigoTerenci <rodrigoterenci@gmail.com>
2021-12-16 00:41:27 +0000

    app.test.js nao passou

--- naruto-quotes-cliente/src/pages/app/App.test.js
+++ naruto-quotes-cliente/src/pages/app/App.test.js
@@ -1,15 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the app with a button, a quote and a img', () => {
+test('renders the app with a button and a img', () => {
   render(<App />);
-  const textEl = screen.findByText(/Speaker/i)
+  const textEl = screen.findByText('Speaker')
   const imgEl = screen.getByRole('img')
   const buttonEl = screen.getByRole('button')
 
   expect(textEl).toBeInTheDocument()
   expect(imgEl).toBeInTheDocument()
   expect(buttonEl).toBeInTheDocument()
-
 
 });
