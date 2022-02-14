e751e1dca3c151b8722955feb0fcde39073b0ed8
rodrigoTerenci <rodrigoterenci@gmail.com>
2022-02-14 22:35:20 +0000

    install and conf. qrcode

--- naruto-quotes-cliente/src/index.js
+++ naruto-quotes-cliente/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import { GlobalStyle } from './components/';
 import App from './pages/app/App';
 import reportWebVitals from './reportWebVitals';
+import QRCode from "react-qr-code";
+
+
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
+    <QRCode value="https://3000-rodrigoterenci-narutoquo-0mtns68rywh.ws-us31.gitpod.io" />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
